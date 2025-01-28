5ad2e19a39c8db749e1e0a1bd352398c92585f6b
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2025-01-27 21:32:58 -0400

    seeder de marcas y carros

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -6,21 +6,11 @@ import { CreateCarDTO, UpdateCarDTO } from './dto';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
-    {
-      id: uuid(),
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      id: uuid(),
-      brand: 'Honda',
-      model: 'Civic',
-    },
-    {
-      id: uuid(),
-      brand: 'Jeep',
-      model: 'Cherokee',
-    },
+    // {
+    //   id: uuid(),
+    //   brand: 'Toyota',
+    //   model: 'Corolla',
+    // }
   ];
 
   public findALl() {
@@ -68,5 +58,9 @@ export class CarsService {
   delete(id: string) {
     this.findOneById(id);
     this.cars = this.cars.filter((car) => car.id !== id);
+  }
+
+  fillCarsWithData(cars: Car[]) {
+    this.cars = cars;
   }
 }
